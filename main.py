aa41d699f24d81e43fe6ac6ff1b06eb95be6fb82
ayinoue <ayinoue@bbtower.co.jp>
2021-08-03 22:45:59 +0900

    20210803 modified

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text=reply_text))
+    TextSendMessage(reply_text))
 
 """ @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
